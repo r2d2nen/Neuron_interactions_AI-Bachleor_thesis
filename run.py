5e8d5d75bf685f2e89234fb7fd3f3cb549396a55
whaibop <whaibop@gmail.com>
2017-02-20 12:10:33 +0100

    Added comments in run.py

--- run.py
+++ run.py
@@ -8,28 +8,36 @@ from ml2017 import ml2017
 from datamanager import Datamanager
 
 def main():
+    """Controls the program flow determined by user input"""
+
+    # Parses arguments supplied by the user. 
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbosity", action="store_true", help="Show data output")
+    
+    # Add a group of arguments that can't coexist
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-f", "--function", action="store", help="Use specified function for GP")
     group.add_argument("-n", "--nsopt", action="store_true", help="Use nsopt calculation for GP")
-    group.add_argument("-l", "--load", action="store_true", help="Use stored data for GP") #Add tag handling
+    group.add_argument("-l", "--load", action="store", help="Use stored data for GP") #Add tag handling
     args = parser.parse_args()
 
-    '''Project class objects'''
+    # Initialize project class objects
     nsopt = ml2017()
     gauss = Gaussfit()
     dm = Datamanager()
 
+    # Read input arguments to get input vector
     X = nsopt.read_ini(args)
-    
+
+    # Do we want to genereate new nsopt values or use specified function.
     if args.nsopt:
         Y = nsopt.get_nsopt_observable()
         Y = np.trim_zeros(Y)
         Y = Y.reshape(len(Y),1)
     else:
         Y = np.sin(X) + np.random.randn(20,1)*0.03
-    
+
+    # Set up GP-processes and plot output after optimization.
     gauss.set_gp_kernel()
     gauss.populate_gp_model(X, Y)
     gauss.optimize()
