5d954f38c8c9165fe85389bff866f579c37a0b2f
whaibop <whaibop@gmail.com>
2017-02-23 16:51:24 +0100

    Fixed population of GPY model

--- gaussfit.py
+++ gaussfit.py
@@ -9,40 +9,45 @@ DEFAULTS = {'kernel': 'RBF', 'input_dim': 1, 'variance': 1., 'lengthscale': 1.}
 class Gaussfit:
     """Handles GPR of input data. """
     def __init__(self):
-        '''Initialize a gaussfit object '''
+        """Initialize a gaussfit object."""
         self.kernel = None
         self.model = None
     
     def set_gp_kernel(self, kernel=DEFAULTS['kernel'], in_dim=DEFAULTS['input_dim'], variance=DEFAULTS['variance'], lengthscale=DEFAULTS['lengthscale']):
-        '''Sets the kernel of this Gaussfit'''
-        '''Need to manually add the different kernels'''
+        """Sets the kernel of this Gaussfit"""
+        """Need to manually add the different kernels"""
         if kernel == 'RBF':
             self.kernel = RBF(input_dim=in_dim, variance=variance, lengthscale=lengthscale)
         else:
             print 'Kernel not recognized'
     
 
-    def populate_gp_model(self, X, Y):
-        '''Creates a model based on given data and kernel.
+    def populate_gp_model(self, observable, lecs, energy=None):
+        """Creates a model based on given data and kernel.
         
         Args:
-        X - numpy array with parameters fit should be done with regard to (
-        Y - numpy array with observable. (nbr_of_observables, different_obserable_types)
-        '''
-        X = X.reshape()
-        if Y.shape()[2]
-        Y = Y.reshape(len(X))
+        observable - numpy array with observable. (1 row for each observable from each lec sample)
+        lecs - numpy array with lec parameters fit should be done with regard to (lec 1 coloum 1 and so on, sample 1 on row 1 and so on)
+        energy - energy values 
+        """
+        # Add row with energies to parameters for fit (c for col if that is that is the right way)
+        if energy is not None:
+        lecs = np.r_(lecs, energy)
+        lecs.transpose()
+
+        observable.transpose()
+        self.model = GPRregression(input_dim=lecs.shape()[0],observable, lecs ,self.kernel)
+
         
-        self.model = GPRegression(X, Y, self.kernel)
 
     def optimize(self, num_restarts=1):
-        '''Optimize the model. TODO: add verbose'''
-        '''Something worng, model doesn't always converge'''
+        """Optimize the model. TODO: add verbose"""
+        """Something worng, model doesn't always converge"""
         self.model.optimize_restarts(num_restarts=num_restarts, messages=True)
     
     def plot(self):
-        '''Plot the GP-model'''
-        '''Plot limits only for 1D-case'''
+        """Plot the GP-model"""
+        """Plot limits only for 1D-case"""
         print(self.model)
         self.model.plot()
         plt.show()
