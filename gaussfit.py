fb2380045996b39d501d27e42192a04d2ae7a8b3
rikard-helgegren <rikard.helgegren@gmail.com>
2017-03-02 16:15:51 +0100

    Cahnged some functionallity in rescale.

--- gaussfit.py
+++ gaussfit.py
@@ -13,6 +13,8 @@ class Gaussfit:
         """Initialize a gaussfit object."""
         self.kernel = None
         self.model = None
+        self.translation = None
+        self.scale = None
     
     def set_gp_kernel(self, kernel=DEFAULTS['kernel'], in_dim=DEFAULTS['input_dim'], variance=DEFAULTS['variance'], lengthscale=DEFAULTS['lengthscale']):
         """Sets the kernel of this Gaussfit"""
@@ -22,7 +24,14 @@ class Gaussfit:
         else:
             print 'Kernel not recognized'
     
-
+    def set_translation(self, translation):
+        self.translation = translation
+    
+    def set_scale(self, scale):
+        self.scale = scale
+        
+        
+        
     def populate_gp_model(self, observable, lecs, energy=None):
         """Creates a model based on given data and kernel.
         
@@ -49,12 +58,12 @@ class Gaussfit:
             so that they are in the interval [-1,1] #Remove 16xnr 
             """
         
-            translation = -(colum.max()+colum.min())/2
-            new_array = colum + translation  #translation all the values
-            scale = new_array.max()
-            if scale == 0: # All values are 0 
+            #translation = -(colum.max()+colum.min())/2
+            new_array = colum + self.translation  #translation all the values
+            #scale = new_array.max()
+            if self.scale == 0: # All values are 0 
                 return new_array
-            new_array =  new_array/scale #scale all the values
+            new_array =  new_array/self.scale #scale all the values
         
             return new_array
             
@@ -72,19 +81,18 @@ class Gaussfit:
         """Rescales the input parameters that Gpy handles,
            so that they are in the interval [-1,1] #Remove 16xnr 
         """
-       
+        if self.scale = None or self.translate = None:
+            print("ERROR: 'rescale' requires scale and translateion. One or both are 'None'.")
+            return inMatrix
+            
         def rescale(colum):
             """Rescales the input parameters that Gpy handles,
             so that they are in the interval [-1,1] #Remove 16xnr 
             """
-            
-            #Dividing by float
-            translation = -(colum.max()+colum.min())/2.0
-            new_array = colum + translation  #translation all the values
-            scale = float(new_array.max())
-            if scale == 0: # All values are 0 
+            new_array = colum + self.translation  #translation all the values
+            if self.scale == 0: # All values are 0 
                 return new_array
-            new_array =  new_array/scale #scale all the values
+            new_array =  new_array/self.scale #scale all the values
         
             return new_array
             
